e39cfcc2f4a96eb59286a19c0fd01d8a8e452ea9
whyj_yj <yoonjaebaek0312@gmail.com>
2021-09-19 12:25:20 +0000

    video crud - create

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,31 +1,43 @@
 import Video from '../models/Video';
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
-  return res.render("home", { pageTitle: "Home", videos });
+	const videos = await Video.find({});
+	return res.render('home', { pageTitle: 'Home', videos });
 };
 export const watch = (req, res) => {
-    const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render('watch', { pageTitle: `Watching`});
+	const { id } = req.params;
+	const video = videos[id - 1];
+	return res.render('watch', { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
-    const { id } = req.params;
-    const video = videos[id - 1];
-    return res.render('edit', { pageTitle: `Editing` });
+	const { id } = req.params;
+	const video = videos[id - 1];
+	return res.render('edit', { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
-    const { id } = req.params;
-    const { title } = req.body;
-    videos[id - 1].title = title;
-    return res.redirect(`/videos/${id}`);
+	const { id } = req.params;
+	const { title } = req.body;
+	videos[id - 1].title = title;
+	return res.redirect(`/videos/${id}`);
 };
 
 export const getUpload = (req, res) => {
-    return res.render('upload', { pageTitle: 'Upload Video' });
+	return res.render('upload', { pageTitle: 'Upload Video' });
 };
 
-export const postUpload = (req, res) => {
-    const { title } = req.body;
-    return res.redirect('/');
+export const postUpload = async (req, res) => {
+	const { title, description, hashtags } = req.body;
+	try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  createdAt: Date,
-  hashtags: [{ type: String }],
-  meta: {
-    views: Number,
-    rating: Number,
-  },
+	title: { type: String, required: true, trim: true, maxLength: 80 },
+	description: { type: String, required: true, trim: true, minLength: 20 },
+	createdAt: { type: Date, required: true, default: Date.now },
+	hashtags: [{ type: String, trim: true }],
+	meta: {
+		views: { type: Number, default: 0, required: true },
+		rating: { type: Number, default: 0, required: true },
+	},
 });
 
-const Video = mongoose.model("Video", videoSchema);
+const Video = mongoose.model('Video', videoSchema);
 export default Video;
