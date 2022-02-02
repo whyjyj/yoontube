b5e2d30583cee8607d1025fc6831fee2e41a04fe
whyjyj <yoonjaebaek0312@gmail.com>
2022-02-02 12:25:35 +0000

    github login

--- README.md
+++ README.md
@@ -3,63 +3,84 @@
 ## Implementation
 
 ### Users
-* [X] User Authentication
-* [ ] Facebook Login
-* [ ] Github Login
-* [ ] User Profile
-* [X] Login
-* [ ] Logout
+
+- [x] User Authentication
+- [ ] Facebook Login
+- [x] Github Login
+- [ ] User Profile
+- [x] Login
+- [ ] Logout
 
 ### Videos
-* [ ] Video Upload
-* [ ] Video Recording
-* [X] Search Video
-* [ ] AJAX Comments
-* [ ] View Count
+
+- [ ] Video Upload
+- [ ] Video Recording
+- [x] Search Video
+- [ ] AJAX Comments
+- [ ] View Count
 
 ### Theory
-* [X] MVC
-* [X] Routing
-* [X] Templates
-* [X] Models
-* [ ] Relationships
+
+- [x] MVC
+- [x] Routing
+- [x] Templates
+- [x] Models
+- [ ] Relationships
 
 ---
 
 ## Update log
 
 ### 2021.09.02 - 2021.09.03
+
 - Routes 설계
 - Controller & Router 틀
 
 ### 2021.09.04 - 2021.09.05
+
 - templates setup
 
 ### 2021.09.10
+
 - GET/POST form Recap
 
 ### 2021.09.11
+
 - Setting Mongo DB & Mongoose
 
 ### 2021.09.12
+
 - Video Model
 
 ### 2021.09.19
+
 - Home controller에 async 적용
 - Video CRUD - Create
 
 ### 2021.09.20
+
 - Video Detail
 - Video CRUD - Update(Edit)
 
 ### 2021.09.21
+
 - Video CRUD - Delete
 
 ### 2021.09.22
+
 - Video CRUD - Read(Search)
 - Create Account
 - Form Validation with status codes
 
 ### 2021.09.24 - 2021.09.26
+
 - cookie, session
 - User Authentication
+
+### 2022.01.25 ~ 2022.02.01
+
+- Review all
+
+### 2022.02.01 - 2022.02.02
+
+- Github Login
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/User';
+import fetch from "node-fetch";
 import bcrypt from 'bcrypt';
 
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
@@ -56,6 +57,82 @@ export const postLogin = async (req, res) => {
 	req.session.loggedIn = true;
 	req.session.user = user;
 	return res.redirect('/');
+};
+
+export const startGithubLogin = (req, res) => {
+	const baseUrl = "https://github.com/login/oauth/authorize";
+	const config = {
+		client_id: process.env.GH_CLIENT,
+		allow_signup: false,
+		scope: "read:user user:email",
+	};
+	const params = new URLSearchParams(config).toString();
+	const finalUrl = `${baseUrl}?${params}`;
+	return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+	const baseUrl = "https://github.com/login/oauth/access_token";
+	const config = {
+		client_id: process.env.GH_CLIENT,
+		client_secret: process.env.GH_SECRET,
+		code: req.query.code,
+	};
+	const params = new URLSearchParams(config).toString();
+	const finalUrl = `${baseUrl}?${params}`;
+	const tokenRequest = await (
+		await fetch(finalUrl, {
+			method: "POST",
+			headers: {
+				Accept: "application/json",
+			},
+		})
+	).json();
+	if ("access_token" in tokenRequest) {
+		const { access_token } = tokenRequest;
+		const apiUrl = "https://api.github.com";
+		const userData = await (
+			await fetch(`${apiUrl}/user`, {
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
+			})
+		).json();
+		console.log(userData);
+		const emailData = await (
+			await fetch(`${apiUrl}/user/emails`, {
+				headers: {
+					Authorization: `token ${access_token}`,
+				},
+			})
+		).json();
+		const emailObj = emailData.find(
+			(email) => email.primary === true && email.verified === true
+		);
+		if (!emailObj) {
+			return res.redirect("/login");
+		}
+		const existingUser = await User.findOne({ email: emailObj.email });
+		if (existingUser) {
+			req.session.loggedIn = true;
+			req.session.user = existingUser;
+			return res.redirect("/");
+		} else {
+			const user = await User.create({
+				name: userData.name,
+				username: userData.login,
+				email: emailObj.email,
+				password: "",
+				socialOnly: true,
+				location: userData.location,
+			});
+			req.session.loggedIn = true;
+			req.session.user = user;
+			return res.redirect("/");
+		}
+	} else {
+		return res.redirect("/login");
+	}
 };
 
 export const edit = (req, res) => res.send('Edit User');
