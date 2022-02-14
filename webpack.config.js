1ac54a7bedb10db4fbf148bdd8187498deff9eec
YoonJae Baek <yoonjaebaek0312@gmail.com>
2022-02-14 12:04:25 +0000

    comment box

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
+const BASE_JS = "./src/client/js/";
+
 module.exports = {
     entry: {
-        main: "./src/client/js/main.js",
-        videoPlayer: "./src/client/js/videoPlayer.js",
+        main: BASE_JS + "main.js",
+    	videoPlayer: BASE_JS + "videoPlayer.js",
+    	commentSection: BASE_JS + "commentSection.js",
     },
     mode: "development",
     watch: true,
