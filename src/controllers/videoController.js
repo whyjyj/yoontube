692f27b28ca7dc6fd15d3db4c0c9c30810d3ae18
YoonJae Baek <yoonjaebaek0312@gmail.com>
2022-02-16 12:16:29 +0000

    finish flash messages

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,7 +31,7 @@ export const getEdit = async (req, res) => {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
-	req.flash("error", "Not authorized");
+    req.flash("error", "Not authorized");
     return res.status(403).redirect("/");
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
@@ -48,7 +48,7 @@ export const postEdit = async (req, res) => {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
-	req.flash("error", "You are not the the owner of the video.");
+    req.flash("error", "You are not the the owner of the video.");
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
@@ -56,6 +56,7 @@ export const postEdit = async (req, res) => {
     description,
     hashtags: Video.formatHashtags(hashtags)
   });
+  req.flash("success", "Changes saved.");
   return res.redirect(`/videos/${id}`);
 };
 
