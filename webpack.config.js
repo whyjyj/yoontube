e922901188fe38248879b2def31fa321183d05f9
YoonJae Baek <yoonjaebaek0312@gmail.com>
2022-03-01 12:05:27 +0000

    recorder setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,38 +4,39 @@ const path = require("path");
 const BASE_JS = "./src/client/js/";
 
 module.exports = {
-    entry: {
-        main: BASE_JS + "main.js",
-    	videoPlayer: BASE_JS + "videoPlayer.js",
-    	commentSection: BASE_JS + "commentSection.js",
-    },
-    mode: "development",
-    watch: true,
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "css/styles.css",
-        }),
+  entry: {
+    main: BASE_JS + "main.js",
+    videoPlayer: BASE_JS + "videoPlayer.js",
+    recorder: "./src/client/js/recorder.js",
+    commentSection: BASE_JS + "commentSection.js",
+  },
+  mode: "development",
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/styles.css",
+    }),
+  ],
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "assets"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
-    output: {
-        filename: "js/[name].js",    
-        path: path.resolve(__dirname, "assets"),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [["@babel/preset-env", { targets: "defaults" }]],
-                    },
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
-            },
-        ],
-    },
+  },
 };
